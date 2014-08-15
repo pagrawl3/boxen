765e244abbe35343c31c43153878e1426b226a46
pagrawl3 <mail@prath.am>
2014-08-15 22:14:59 +0530

    first try for a config

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,27 +53,102 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
   include git
-  include hub
-  include nginx
-
-  # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
+  git::config::global {
+      'user.name':     value => 'pagrawl3';
+      'user.email':    value => 'mail@prath.am';
+      'core.editor':   value => 'subl';
+      'color.ui':      value => 'true';
   }
 
-  # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
+  include hub
 
-  # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  # fail if FDE is not enabled
+  # if $::root_encrypted == 'no' {
+  #   fail('Please enable full disk encryption and try again')
+  # }
+
+  # node versions
+  # include nodejs::v0_6
+  # include nodejs::v0_8
+  include nodejs::v0_10
+  include alfred
+  include bartender
+  include spotify
+  include dropbox
+  include mongodb
+  include flux
+  include mplayerx
+  include cinch
+  include imageoptim
+
+  include sublime_text::v2
+  sublime_text::v2::package { 'Emmet':
+    source => 'sergeche/emmet-sublime'
+  }
+  sublime_text_2::package { 'Jade':
+      source => 'miksago/jade-tmbundle'
+  }
+  sublime_text_2::package { 'SublimeAllAutocomplete':
+    source => 'alienhard/SublimeAllAutocomplete'
+  }
+  sublime_text_2::package { 'LESS':
+    source => 'danro/Less-sublime'
+  }
+  sublime_text_2::package { 'Color Scheme - Solarized':
+    source => 'altercation/solarized'
+  }
+  file { "${boxen::config::homedir}/bin/subl":
+    ensure  => link,
+    target  => "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+  }
+
+  include chrome
+  include wget
+  include prezto
+
+  repository {
+      "dotfiles":
+        source   => 'dliggat/dotfiles',
+        path     => "${::boxen_srcdir}/dotfiles";
+  }
+
+  $homedir = "/Users/${::boxen_user}"
+  $base = "/Users/${::boxen_user}/Library/Application Support"
+
+  file { "${homedir}/.bash_profile":
+    ensure  => link,
+    target  => "${::boxen_srcdir}/dotfiles/bash_profile",
+    require => Repository["dotfiles"],
+  }
+
+  file { "${homedir}/.bashrc":
+    ensure  => link,
+    target  => "${::boxen_srcdir}/dotfiles/bashrc",
+    require => Repository["dotfiles"],
+  }
+
+  # file { "${homedir}/.zshrc":
+  #   ensure  => link,
+  #   target  => "${::boxen_srcdir}/dotfiles/zshrc",
+  #   require => Repository["dotfiles"],
+  # }
+
+  file { "${homedir}/.gitignore":
+    ensure  => link,
+    target  => "${::boxen_srcdir}/dotfiles/gitignore",
+    require => Repository["dotfiles"],
+  }
+
+  exec { 'Idempotent creation of User preferences directory':
+    command => "mkdir -p '${base}/Sublime Text 2/Packages/User'"
+  }
+
+  file { "${base}/Sublime Text 2/Packages/User/Preferences.sublime-settings":
+    ensure  => link,
+    target  => "${::boxen_srcdir}/dotfiles/pref.sublime-settings",
+    require => Repository["dotfiles"],
+  }
 
   # common, useful packages
   package {
